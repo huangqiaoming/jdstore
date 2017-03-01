926414e3c7d61271316b17eba70cb1d8b2206bba
huangqiaoming <carolinehuang.aiesec@gmail.com>
2017-03-01 10:19:20 +0800

    adjust config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'https://ancient-tor-86693.herokuapp.com/'}
+  config.action_mailer.default_url_options = { :host => 'https://irobot-jdstore.herokuapp.com/'}
 
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
